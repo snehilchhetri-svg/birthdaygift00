df84b7b46cb26d8f5b72b680044f5aa7c4a67251
snehilchhetri-svg <snehilchhetri@gmail.com>
2026-05-17 22:57:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date('may 15, 2026 00:00:00').getTime(),
+let countDown = new Date('may 18, 2026 00:00:00').getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
